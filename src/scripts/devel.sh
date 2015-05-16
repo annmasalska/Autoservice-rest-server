ae67ed46e7e452bc76f435237cdb84e7ea28023d
amasalskaya <mas.anna25@gmail.com>
2015-05-17 00:38:17 +0300

    remove hidden files

--- src/scripts/devel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting exams-thrift-server in development mode..."
-
-# find jar no matter what the root dir name
-SCRIPT_DIR=$(cd `dirname "$0"`; pwd)
-ROOT_DIR=`dirname "$SCRIPT_DIR"`
-
-java -server -Xmx1024m -Dstage=development -jar exams-thrift-server_2.10-@VERSION@.jar
